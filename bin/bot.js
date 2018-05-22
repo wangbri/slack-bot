9323d25dbfdaa92178f9ebb9f98513b86484d196
Wang <brwang@lm-ast-21017535.paypalcorp.com>
2018-05-22 10:39:38 -0500

    add node-slack-sdk with slackbot

--- bin/bot.js
+++ bin/bot.js
@@ -6,8 +6,7 @@ var token = 'xoxb-268842301509-368712201463-u0V8vR8bH9XSq18FX3rvswJs';
 var name = process.env.BOT_NAME;
 
 var supportbot = new SupportBot({
-    token: token,
-    name: name
+	token: token
 });
 
 supportbot.run();
